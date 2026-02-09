aa8e77897388009bd7d57807cfe425c2b9dc9ce1
Rhmt codeAi engineering <rohmatulloh.3609@gmail.com>
2026-02-09 13:39:46 +0700

    Update proxy.js

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -10,17 +10,26 @@ export default async function handler(req, res) {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36",
+        Referer: "https://downr.org/",
       },
     });
 
-    const contentType = response.headers["content-type"] || "application/octet-stream";
+    const contentType =
+      response.headers["content-type"] || "application/octet-stream";
+
+    let ext = "bin";
+    if (contentType.includes("video")) ext = "mp4";
+    else if (contentType.includes("audio")) ext = "mp3";
+    else if (contentType.includes("image")) ext = "jpg";
+
+    const filename = `download-${Date.now()}.${ext}`;
 
     res.setHeader("Content-Type", contentType);
-    res.setHeader("Content-Disposition", "attachment");
+    res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
 
     response.data.pipe(res);
   } catch (err) {
     console.error(err);
     res.status(500).send("Gagal download file");
   }
-}
+} 
